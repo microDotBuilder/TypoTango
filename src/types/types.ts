0224efa3143f56498e7c003312fc3fcbb5e0eb89
boringtutor <micro.builder.io@gmail.com>
2025-10-07 20:23:44 -0700

    Add appState constants and type definition to types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,10 @@ import type { TIMERSTATE } from "../consts";
 
 export type timerType = keyof typeof TIMERSTATE;
 export type timerValue = (typeof TIMERSTATE)[timerType];
+
+export const appState = {
+  IDLE: "IDLE",
+  TYPING: "TYPING",
+  FINISHED: "FINISHED",
+};
+export type appState = (typeof appState)[keyof typeof appState];
