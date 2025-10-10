535611d4b2abe52ae89b701d86515c72314fedc2
boringtutor <micro.builder.io@gmail.com>
2025-10-09 20:26:56 -0700

    Enhance Home component with dynamic timer display and input handling; update NavBar and Footer visibility based on app state

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,22 +6,35 @@ import "./Home.css";
 import type { timerValue } from "../types/types";
 import { TIMERSTATE } from "../consts";
 import MainComponent from "../Main/main-component";
+import { appState } from "../consts";
+import { useAppState } from "../provider/appStateProvider";
 
 function Home() {
   const [timer, setTimer] = useState<timerValue>(TIMERSTATE["15seconds"]);
+  const currentAppState = useAppState();
+  console.log(currentAppState.state.current);
   return (
     <div className="header-container ">
       <NavBar />
-      {/* <div>{timer}</div> */}
-      <div className="timer-nav">
-        <TimerNav setTimer={setTimer} currentTimer={timer} />
-      </div>
+      {currentAppState.state.current === appState.TYPING ? (
+        <div className="timer-nav-display">{timer}</div>
+      ) : (
+        <div className="timer-nav">
+          <TimerNav setTimer={setTimer} currentTimer={timer} />
+        </div>
+      )}
+
       <div className="home-content">
         <MainComponent timer={timer} />
       </div>
-      <div className="footer-content">
-        <Footer />
-      </div>
+
+      {currentAppState.state.current === appState.TYPING ? (
+        <></>
+      ) : (
+        <div className="footer-content">
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
--- src/Main/main-component.tsx
+++ src/Main/main-component.tsx
@@ -1,17 +1,33 @@
 import "./main-component.css";
 import { getRandomInitialState } from "./utils/getPassage";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import type { timerValue } from "../types/types";
+import { useAppState } from "../provider/appStateProvider";
 
 function MainComponent({ timer }: { timer: timerValue }) {
   const [initialState, setInitialState] = useState<
     { char: string; wordState: string }[]
   >([]);
+  const { appState, setAppState } = useAppState();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputCharacters, setInputCharacters] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAppState(appState.TYPING);
+    setInputCharacters(e.target.value);
+  };
+  const onLoad = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   useEffect(() => {
     setInitialState(getRandomInitialState(timer));
   }, [timer]);
   return (
-    <div className="main-component">
+    <div className="main-component" onClick={onLoad}>
       <div className="paragraph">
         {initialState.map((item, index) => {
           return (
@@ -21,6 +37,13 @@ function MainComponent({ timer }: { timer: timerValue }) {
           );
         })}
       </div>
+      <input
+        className="input"
+        ref={inputRef}
+        type="text"
+        value={inputCharacters}
+        onChange={handleInputChange}
+      />
     </div>
   );
 }
